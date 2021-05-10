1e3860226e09ae454ae96d5180d27e8369c6fb30
Emmanuel Adebayo <eadebayo15@gmail.com>
2021-05-11 00:26:42 +0100

    Finished the app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
 import data from "./services/textService";
+import Input from "./components/Input";
 
 import "./App.css";
-import Input from "./components/Input";
 
 function App() {
   const [count, setCount] = useState(0);
