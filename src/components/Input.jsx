ea10904b2a90187c93633b02ad272aa41787a3b0
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-26 03:16:18 +0100

    fix: format code

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -4,7 +4,12 @@ const Input = ({ name, label, ...rest }) => {
   return (
     <div>
       <label htmlFor={name}>{label}</label>
-      <input {...rest} type='number' name={name} id={name} />
+      <input
+        {...rest}
+        type='number'
+        name={name}
+        id={name}
+      />
     </div>
   );
 };
