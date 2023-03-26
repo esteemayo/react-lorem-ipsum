268301416eb416f1979c1b14772bdc43fd745bf9
Emmanuel Adebayo <eadebayo15@gmail.com>
2023-03-26 03:24:00 +0100

    fix: modify

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React, { useState } from 'react';
 import data from './data';
 import Input from 'components/Input';
 
-
 function App() {
   const [count, setCount] = useState(0);
   const [text, setText] = useState([]);
